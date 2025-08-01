acd2b86f41733aa7a914482a836cc3f6b5280c01
David E. Narváez <david.narvaez@computer.org>
2025-08-01 15:00:29 +0200

    Properly use the Sym2 namespace

--- Ramsey2lemmas/Sym2.lean
+++ Ramsey2lemmas/Sym2.lean
@@ -1,12 +1,14 @@
 import Mathlib.Data.Sym.Sym2
 
-instance Sym2.decBall {α : Type} (p : α → Prop) [pdec : DecidablePred p] (s : Sym2 α) : Decidable (∀ x ∈ s, p x) := @Sym2.rec α (λ s ↦ Decidable (∀ x ∈ s, p x)) (λ (ql, qr) ↦ match pdec ql with | isFalse h => isFalse (by simp; intro; contradiction) | isTrue hql => match pdec qr with | isFalse h => isFalse (by simp [h]) | isTrue hqr => isTrue (by simp; tauto)) (by aesop (add safe (by congr!))) s
+namespace Sym2
 
-instance Sym2.decBex {α : Type} (p : α → Prop) [pdec : DecidablePred p] (s : Sym2 α) : Decidable (∃ x ∈ s, p x) := @Sym2.rec α (λ s ↦ Decidable (∃ x ∈ s, p x)) (λ (ql, qr) ↦ match pdec ql with | isTrue h => isTrue (by simp [h]) | isFalse hql => match pdec qr with | isTrue h => isTrue (by simp [h]) | isFalse hqr => isFalse (by simp; tauto)) (by aesop (add safe (by congr!))) s
+instance decBall {α : Type} (p : α → Prop) [pdec : DecidablePred p] (s : Sym2 α) : Decidable (∀ x ∈ s, p x) := @Sym2.rec α (λ s ↦ Decidable (∀ x ∈ s, p x)) (λ (ql, qr) ↦ match pdec ql with | isFalse h => isFalse (by simp; intro; contradiction) | isTrue hql => match pdec qr with | isFalse h => isFalse (by simp [h]) | isTrue hqr => isTrue (by simp; tauto)) (by aesop (add safe (by congr!))) s
+
+instance decBex {α : Type} (p : α → Prop) [pdec : DecidablePred p] (s : Sym2 α) : Decidable (∃ x ∈ s, p x) := @Sym2.rec α (λ s ↦ Decidable (∃ x ∈ s, p x)) (λ (ql, qr) ↦ match pdec ql with | isTrue h => isTrue (by simp [h]) | isFalse hql => match pdec qr with | isTrue h => isTrue (by simp [h]) | isFalse hqr => isFalse (by simp; tauto)) (by aesop (add safe (by congr!))) s
 
 -- NOTE: This is like Quot.out but I think with the (very powerful)
 -- assumption [LinearOrder α], it is computable
-def Sym2.toOrderedPair {α : Type} [LinearOrder α] (s : Sym2 α) : α × α := Sym2.rec (λ p ↦ (min p.fst p.snd, max p.fst p.snd)) (by
+def toOrderedPair {α : Type} [LinearOrder α] (s : Sym2 α) : α × α := Sym2.rec (λ p ↦ (min p.fst p.snd, max p.fst p.snd)) (by
   simp
   intros aₗ aᵣ bₗ bᵣ abeq
   cases abeq with
@@ -14,14 +16,14 @@ def Sym2.toOrderedPair {α : Type} [LinearOrder α] (s : Sym2 α) : α × α := 
   | inr abeq => simp [abeq.left, abeq.right, min_comm bᵣ bₗ, max_comm bᵣ bₗ]
 ) s
 
-theorem Sym2.exists_mem_pair {P : α → Prop} {a b : α} : (∃ x ∈ s(a, b), P x) ↔ P a ∨ P b := by
+lemma exists_mem_pair {P : α → Prop} {a b : α} : (∃ x ∈ s(a, b), P x) ↔ P a ∨ P b := by
   simp only [mem_iff, exists_eq_or_imp, exists_eq_left]
 
-lemma Sym2.toOrderedPair_repr {α : Type} [LinearOrder α] (s : Sym2 α) : s = s(s.toOrderedPair.fst, s.toOrderedPair.snd) := by
+lemma toOrderedPair_repr {α : Type} [LinearOrder α] (s : Sym2 α) : s = s(s.toOrderedPair.fst, s.toOrderedPair.snd) := by
   cases s with
   | h x y => simp [toOrderedPair, Sym2.rec, Quot.rec, LinearOrder.le_total]
 
-lemma Sym2.toOrderedPair_inj {α : Type} [LinearOrder α] : Function.Injective (@Sym2.toOrderedPair α _) := by
+lemma toOrderedPair_inj {α : Type} [LinearOrder α] : Function.Injective (@Sym2.toOrderedPair α _) := by
   intros a b
   intros abord
   trans Sym2.mk a.toOrderedPair
@@ -31,7 +33,7 @@ lemma Sym2.toOrderedPair_inj {α : Type} [LinearOrder α] : Function.Injective (
     · symm
       apply Sym2.toOrderedPair_repr
 
-lemma Sym2.toOrderedPair_IsDiag_of_eq {α : Type} [inst : LinearOrder α] : ∀ {s t : Sym2 α}, s.toOrderedPair = t.toOrderedPair.swap → s.IsDiag ∧ t.IsDiag := by
+lemma toOrderedPair_IsDiag_of_eq {α : Type} [inst : LinearOrder α] : ∀ {s t : Sym2 α}, s.toOrderedPair = t.toOrderedPair.swap → s.IsDiag ∧ t.IsDiag := by
   intros s
   cases s with
   | h sx sy =>
@@ -55,3 +57,5 @@ lemma Sym2.toOrderedPair_IsDiag_of_eq {α : Type} [inst : LinearOrder α] : ∀ 
           simp_all [inst.le_antisymm _ _ sylex.left sylex.right]
         · simp at sylex sxle ⊢
           apply inst.le_antisymm <;> assumption
+
+end Sym2
