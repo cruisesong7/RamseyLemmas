47fff304c8ec15faa9f3038346e370e76a95a4b2
David E. Narváez <david.narvaez@computer.org>
2025-08-16 20:12:08 +0200

    Implemented the Bron Kerbosch Algorithm
    This allows us to calculate the clique/independent set number of
    graphs quickly, so we can prove the witnesses for the inequalities
    correct.

---
+++ Ramsey2lemmas/BronKerbosch.lean
@@ -0,0 +1,230 @@
+import Mathlib.Combinatorics.SimpleGraph.Clique
+import Mathlib.Data.FinEnum
+
+-- NOTE: For List.sizeOf_filter, needed to prove termination of Bron_Kerbosch
+import Trestle.Upstream.ToStd
+
+namespace List
+
+lemma length_wf {α : Type} : WellFounded (λ (x y : List α) ↦ x.length < y.length) := by
+  suffices acc_len : ∀ (n : ℕ) (l : List α), l.length ≤ n → Acc (λ (x y : List α) ↦ x.length < y.length) l by
+    constructor
+    intro l
+    apply acc_len l.length
+    simp
+  intro n
+  induction n with
+  | zero =>
+    simp
+    constructor
+    intros y yltx
+    simp at yltx
+  | succ m ih =>
+    intros l
+    cases l with
+    | nil =>
+      simp
+      constructor
+      intros y yltx
+      simp at yltx
+    | cons h t =>
+      simp
+      intros tlen
+      constructor
+      intros y ylen
+      simp [Nat.lt_add_one_iff] at ylen
+      apply ih
+      trans t.length <;> assumption
+
+end List
+
+namespace SimpleGraph
+
+def Bron_Kerbosch_full {α : Type} [Fintype α] [DecidableEq α] (G : SimpleGraph α) [DecidableRel G.Adj] {R P X : List α} (Pnd : P.Nodup) (RP : R.Disjoint P) (RX : R.Disjoint X) (PX : P.Disjoint X) : List (Finset α) :=
+  match P with
+  | [] =>
+    match X with
+    | [] => [R.toFinset]
+    | h :: t => []
+  | v :: P' =>
+    Bron_Kerbosch_full G (by simp at Pnd; apply Pnd.right.filter) (by simp [List.Disjoint] at RP ⊢; intros a ainR ainP' av; cases (RP ainR).right ainP' : (v :: R).Disjoint (P'.filter (λ u ↦ decide (G.Adj u v)))) (by simp [List.Disjoint] at RX ⊢; intros a ainR ainX; cases RX ainR ainX : (v :: R).Disjoint (X.filter (λ u ↦ decide (G.Adj u v)))) (by simp [List.Disjoint] at PX ⊢; intros a ainP' av ainX; cases (PX.right a ainP') ainX : (P'.filter (λ u ↦ decide (G.Adj u v))).Disjoint (X.filter (λ u ↦ decide (G.Adj u v)))) ++ (Bron_Kerbosch_full G (by simp at Pnd; exact Pnd.right) (by simp [List.Disjoint] at RP ⊢; intros a ainR ainP'; cases (RP ainR).right ainP' : R.Disjoint P') (by simp [List.Disjoint] at RP RX ⊢; intros a ainR; apply And.intro; exact (RP ainR).left; apply RX; exact ainR : R.Disjoint (v :: X)) (by simp [List.Disjoint] at Pnd RP PX ⊢; intros a ainP'; apply And.intro; intro aeqv; rw [aeqv] at ainP'; cases Pnd.left ainP'; exact PX.right a ainP' : P'.Disjoint (v :: X)))
+
+-- lemma Bron_Kerbosch_clique {α : Type} [Fintype α] [DecidableEq α] (G : SimpleGraph α) [DecidableRel G.Adj] {R P X : List α} (Pnd : P.Nodup) (RP : R.Disjoint P) (RX : R.Disjoint X) (PX : P.Disjoint X) : (G.IsClique R.toFinset) → (∀ v ∈ P, ∀ u ∈ R, G.Adj u v) → ∀ S ∈ Bron_Kerbosch_full G Pnd RP RX PX, G.IsClique S := by
+--   induction P using WellFounded.induction List.length_wf generalizing R X with
+--   | h P ih =>
+--     intros Rclique Radj S SBK
+--     cases P with
+--     | nil =>
+--       cases X with
+--       | nil =>
+--         simp [Bron_Kerbosch_full] at SBK Rclique
+--         simpa [SBK]
+--       | cons u X' =>
+--         simp [Bron_Kerbosch_full, Finset.empty] at SBK
+--     | cons v P' =>
+--       simp [Bron_Kerbosch_full] at SBK
+--       cases SBK with
+--       | inl SBKRec =>
+--         apply ih (P'.filter (λ u ↦ decide (G.Adj u v))) (by simp [Nat.lt_add_one_iff, List.length_filter_le]) (by simp at Pnd; apply List.Pairwise.filter; exact Pnd.right) (by simp [List.Disjoint] at RP ⊢; intros a ainR ainP'; cases (RP ainR).right ainP' : (v :: R).Disjoint (P'.filter (λ u ↦ decide (G.Adj u v)))) (by simp [List.Disjoint]; intros _ ainR ainX; cases RX ainR ainX : (v :: R).Disjoint (X.filter (λ u ↦ decide (G.Adj u v))))
+--         · simp at Rclique Radj ⊢
+--           apply And.intro
+--           · exact Rclique
+--           · intros u uinR vnequ
+--             rw [G.adj_comm]
+--             exact Radj.left u uinR
+--         · simp
+--           intros u uinP' uv
+--           apply And.intro
+--           · rw [G.adj_comm]
+--             exact uv
+--           · intros w winR
+--             simp at Radj
+--             exact Radj.right u uinP' w winR
+--         · exact SBKRec
+--       | inr SBKRec =>
+--         apply ih P' (by simp) (by simp at Pnd; exact Pnd.right) (by simp at RP; exact RP.right : R.Disjoint P') (by simp at RP; simp [RP.left, RX] : R.Disjoint (v :: X))
+--         · exact Rclique
+--         · simp at Radj
+--           exact Radj.right
+--         · exact SBKRec
+
+lemma Bron_Kerbosch_maximal_clique {α : Type} [Fintype α] [deceq : DecidableEq α] (G : SimpleGraph α) [DecidableRel G.Adj] {R P X : List α} (Pnd : P.Nodup) (RP : R.Disjoint P) (RX : R.Disjoint X) (PX : P.Disjoint X) : (G.IsClique R.toFinset) → (∀ (v : α), v ∈ (P.toFinset ∪ X.toFinset) ↔ ∀ u ∈ R, G.Adj v u) → ∀ (S : Finset α), S ∈ Bron_Kerbosch_full G Pnd RP RX PX ↔ (R.toFinset ⊆ S ∧ S ⊆ R.toFinset ∪ P.toFinset ∧ Maximal G.IsClique S) := by
+  induction P using WellFounded.induction List.length_wf generalizing R X with
+  | h P ih =>
+    intros Rclique Radj S
+    cases P with
+    | nil =>
+      cases X with
+      | nil =>
+        simp [Bron_Kerbosch_full, Finset.Subset.antisymm_iff] at Radj ⊢
+        rw [and_comm]
+        simp
+        intros RsubS SsubR
+        rw [← subset_antisymm RsubS SsubR]
+        simp [-List.coe_toFinset, Rclique, SimpleGraph.isMaximalClique_iff]
+        intros T TClique RsubT v vinT
+        obtain ⟨u, ⟨uinR, unotadjv⟩⟩ := Radj v
+        cases deceq v u with
+        | isTrue vequ => simpa [vequ]
+        | isFalse vnequ =>
+          rw [G.adj_comm] at unotadjv
+          cases unotadjv (TClique (RsubT (by simp; exact uinR)) vinT (Ne.intro vnequ).symm)
+      | cons v X' =>
+        simp [Bron_Kerbosch_full, Finset.empty, Maximal, Set.subset_def] at Radj ⊢
+        intros RsubS SsubR Sclique
+        rw [← subset_antisymm RsubS SsubR]
+        use (insert v R.toFinset)
+        apply And.intro
+        · simp [-List.coe_toFinset, Rclique]
+          have vadj := Radj v
+          simp at vadj
+          intros u uinR vnequ
+          exact vadj u uinR
+        · apply And.intro
+          · tauto
+          · use v
+            simp at RX ⊢
+            exact RX.left
+    | cons v P' =>
+      simp [Bron_Kerbosch_full] at Radj ⊢
+      rw [ih (P'.filter (λ u ↦ decide (G.Adj u v))) (by simp [Nat.lt_add_one_iff, List.length_filter_le]) (by simp at Pnd; apply List.Pairwise.filter; exact Pnd.right) (by simp [List.Disjoint] at RP ⊢; intros a ainR ainP'; cases (RP ainR).right ainP' : (v :: R).Disjoint (P'.filter (λ u ↦ decide (G.Adj u v)))) (by simp [List.Disjoint]; intros _ ainR ainX; cases RX ainR ainX : (v :: R).Disjoint (X.filter (λ u ↦ decide (G.Adj u v)))) (by simp [List.Disjoint] at PX ⊢; intros a ainP' _ ainX; cases (PX.right a ainP') ainX) (by simp [-List.coe_toFinset, Rclique]; have vadj := Radj v; simp at vadj; intros u uinR; simp [vadj u uinR]) (by simp; intros u; rw [← or_and_right, and_comm]; simp [← Radj u]; intros uv ueqv; simp [ueqv] at uv) S]
+      rw [ih P' (by simp) (by simp at Pnd; exact Pnd.right) (by simp at RP; exact RP.right : R.Disjoint P') (by simp at RP ⊢; simp [RP.left, RX] : R.Disjoint (v :: X)) (by simp at Pnd PX ⊢; simp [Pnd.left, PX.right]) Rclique (by simp; exact Radj) S]
+      rw [← and_assoc, ← and_assoc, ← and_assoc, ← or_and_right]
+      simp [Finset.subset_iff]
+      intros SMax
+      apply Iff.intro
+      · intros SProp
+        cases SProp with
+        | inl Sv =>
+          apply And.intro
+          · intros u uinR
+            exact Sv.left.right u uinR
+          · intros u uinS
+            have := Sv.right uinS
+            tauto
+        | inr Sv =>
+          apply And.intro
+          · exact Sv.left
+          · intros u uinS
+            have := Sv.right uinS
+            tauto
+      · intros SProp
+        cases Finset.decidableMem v S with
+        | isTrue vinS =>
+          left
+          apply And.intro
+          · simp [vinS]
+            intros u uinR
+            exact SProp.left uinR
+          · intros u uinS
+            cases deceq u v with
+            | isTrue ueqv =>
+              simp [ueqv]
+            | isFalse uneqv =>
+              right
+              cases SProp.right uinS with
+              | inl ueqv =>
+                contradiction
+              | inr uRP' =>
+                cases uRP' with
+                | inl uinR =>
+                  left
+                  exact uinR
+                | inr uinP' =>
+                  right
+                  simp [uinP', SMax.left uinS vinS uneqv]
+        | isFalse vnotinS =>
+          right
+          apply And.intro
+          · exact SProp.left
+          · intros u uinS
+            cases SProp.right uinS with
+            | inl ueqv =>
+              simp [ueqv] at uinS
+              contradiction
+            | inr _ =>
+              assumption 
+
+-- NOTE: Here we specialize to FinEnum to make this definition
+-- computable/decidable, but it all applies just the same for Fintype
+def Bron_Kerbosch {α : Type} [fe : FinEnum α] (G : SimpleGraph α) [DecidableRel G.Adj] := Bron_Kerbosch_full G fe.nodup_toList (List.disjoint_nil_left fe.toList) (List.disjoint_nil_left []) (List.disjoint_nil_right fe.toList)
+
+-- lemma Bron_Kerbosch_correct {α : Type} [Fintype α] [DecidableEq α] (G : SimpleGraph α) [DecidableRel G.Adj] : ∀ (S : Finset α), Maximal G.IsClique ↑S ↔ S ∈ Bron_Kerbosch_full G Finset.univ.nodup_toList (List.disjoint_nil_left Finset.univ.toList) (List.disjoint_nil_left []) Finset.univ.toList.disjoint_nil_right := by
+--   intros S
+--   rw [Bron_Kerbosch_maximal_clique] <;> simp
+
+-- lemma Bron_Kerbosch_maximum_clique {α : Type} [Fintype α] [DecidableEq α] (G : SimpleGraph α) [DecidableRel G.Adj] : ∀ (S : Finset α), G.IsMaximumClique S → S ∈ Bron_Kerbosch_full G Finset.univ.nodup_toList (List.disjoint_nil_left Finset.univ.toList) (List.disjoint_nil_left []) Finset.univ.toList.disjoint_nil_right := by
+--   intros S SMum
+--   have SMal := SMum.isMaximalClique
+--   rw [Bron_Kerbosch_correct] at SMal
+--   exact SMal
+
+lemma Bron_Kerbosch_correct {α : Type} [FinEnum α] (G : SimpleGraph α) [DecidableRel G.Adj] : ∀ (S : Finset α), Maximal G.IsClique ↑S ↔ S ∈ Bron_Kerbosch G := by
+  intros S
+  rw [Bron_Kerbosch, Bron_Kerbosch_maximal_clique, Finset.subset_iff, Finset.subset_iff] <;> simp
+
+lemma Bron_Kerbosch_maximum_clique {α : Type} [FinEnum α] (G : SimpleGraph α) [DecidableRel G.Adj] : ∀ (S : Finset α), G.IsMaximumClique S → S ∈ Bron_Kerbosch G := by
+  intros S SMum
+  have SMal := SMum.isMaximalClique
+  rw [Bron_Kerbosch_correct] at SMal
+  exact SMal
+
+lemma Bron_Kerbosch_cliqueNum {α : Type} [FinEnum α] (G : SimpleGraph α) [DecidableRel G.Adj] : G.cliqueNum = ((Bron_Kerbosch G).map Finset.card).max?.getD 0 := by
+  have cliqueNumMem : G.cliqueNum ∈ ((Bron_Kerbosch G).map Finset.card) ∧ ∀ (b : ℕ), b ∈ ((Bron_Kerbosch G).map Finset.card) → b ≤ G.cliqueNum := by
+    simp
+    · apply And.intro
+      · obtain ⟨S, SMum⟩ := G.maximumClique_exists
+        have Smem := SMum.isMaximalClique
+        rw [Bron_Kerbosch_correct] at Smem
+        use S
+        simp [Smem, G.maximumClique_card_eq_cliqueNum S SMum]
+      · intros S SMal
+        rw [← Bron_Kerbosch_correct] at SMal
+        exact SMal.left.card_le_cliqueNum
+  -- NOTE: This line will change in v4.23.0-rc1 so let's leave the try in for now
+  rw [← List.max?_eq_some_iff] at cliqueNumMem <;> try simp
+  · simp [cliqueNumMem]
+  · intros a b
+    exact Nat.le_total b a
+
+end SimpleGraph
--- Ramsey2lemmas/Inequalities.lean
+++ Ramsey2lemmas/Inequalities.lean
@@ -1,6 +1,13 @@
 import Mathlib.Data.Nat.Basic
 
 import Ramsey2lemmas.Theory
+import Ramsey2lemmas.BronKerbosch
+
+import FormalRamsey.Encodings.CNF.RamseyEncoder
+
+import Trestle.Model.PropFun
+
+open Trestle Model PropFun
 
 @[simp]
 lemma Fin.val_three (n : Nat) : (3 : Fin (n + 4)).val = 3 := rfl
@@ -34,11 +41,6 @@ lemma sᵢ_vanish {N x y : ℕ} {G : SimpleGraph (Fin N.succ)} [DecidableRel G.A
     simp [Nat.sub_eq_iff_eq_add (Nat.le_of_lt vdegpos)] at vdeg
     simp [vdeg, Nat.add_one_le_iff, Nat.sub_add_comm (G.minDegree_le_degree v), RamseyOld₂] at σltd
 
--- example (h1 : 27 * e ≥ 85 * s0 + 80 * s1 + 75 * s2 + 75 * s3)
--- (h1 : 2 * e ≤ 7 * s0 + 6 * s1 + 5 * s2 + 4 * s3)
--- (h3 : s0 + s1 + s2 + s3 = 27) :
--- e ≥ 80 := by linarith
-
 theorem R37 : RamseyOld 3 7 = 22 := sorry
 
 lemma e37_18 : e 3 7 18 ≥ 36 := by sorry
@@ -46,46 +48,42 @@ lemma e37_19 : e 3 7 19 ≥ 44 := by sorry
 lemma e37_20 : e 3 7 20 ≥ 50 := by sorry
 lemma e37_21 : e 3 7 21 ≥ 59 := by sorry
 
-lemma Ineq₉_helper {G : SimpleGraph (Fin 27)} [DecidableRel G.Adj] (hxy: G.isXYGraph 3 8) : G.edgeFinset.card ≥ 80 := by
-  have h1 := Prop₄ hxy
-  simp [-Set.toFinset_card] at h1
-  have σub : σ_G hxy < 4 := by
-    simp +arith [σ_G, RamseyOld₂]
-    obtain ⟨v, vmindeg⟩ := G.exists_minimal_degree_vertex
-    have := (Lemma₂ G v hxy).right
-    simp +arith [R37, ← vmindeg] at this
-    assumption
-  have h2 := G_degreeCount_eq G hxy
-  have h3 := G_vertCount_eq G hxy
-  have bilinearlb := σ_helper hxy σub (λ d ↦ (e 3 7 (26 - vᵢ 2 7 d - 1) + (vᵢ 2 7 d)^2) * (G.sᵢ 2 7 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish hxy _ 4; assumption; simp [R37])
-  have vertexlb := σ_helper hxy σub (λ d ↦  1 * (G.sᵢ 2 7 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish hxy _ 4; assumption; simp [R37])
-  have degreelb := σ_helper hxy σub (λ d ↦ (vᵢ 2 7 d) * (G.sᵢ 2 7 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish hxy _ 4; assumption; simp [R37])
-  simp at bilinearlb vertexlb degreelb
-  -- NOTE: Using simp here maxes out hearbeats
-  simp_rw [bilinearlb, Finset.sum_range, Fin.sum_univ_four, vᵢ, RamseyOld₂ 7] at h1
-  simp_rw [Nat.mul_comm, degreelb, G.sum_degrees_eq_twice_card_edges, Finset.sum_range, Fin.sum_univ_four, vᵢ, RamseyOld₂] at h2
-  simp_rw [vertexlb, Finset.sum_range, Fin.sum_univ_four] at h3
-  simp at h1 h2 h3 ⊢
-  nlinarith [e37_18, e37_19, e37_20, e37_21]
-
--- theorem Ineq₉:
--- e 3 8 26 ≥ 80 := by
---   simp only [e]
---   sorry
---   -- apply le_csInf
---   -- simp [Set.Nonempty, -Set.toFinset_card, isXYGraph]
---   -- use sorry
---   -- let G := readG6 "Z????CDO?a@PHA_HcE_dc`PCXQ@PoSWo@_cDS_YQQB_Qo?TLSO?q_g?{p?_?"
---   -- use G
---   -- apply And.intro
---   -- have : G.cliqueFinset 3 = Finset.empty := by native_decide
---   -- native_decide
-
---   -- sorry
---   -- simp [-Set.toFinset_card]
---   -- intro N G hxy _ hn
---   -- have := Ineq₉_helper hxy h1 h2 h3 h4
---   -- omega
+theorem Ineq₉: e 3 8 26 ≥ 80 := by
+  simp only [e]
+  apply le_csInf
+  · let G := readG6 "Z????CDO?a@PHA_HcE_dc`PCXQ@PoSWo@_cDS_YQQB_Qo?TLSO?q_g?{p?_?"
+    use G.edgeFinset.card
+    simp
+    use G
+    apply And.intro
+    · unfold SimpleGraph.isXYGraph
+      rw [G.Bron_Kerbosch_cliqueNum, ← G.cliqueNum_compl, Gᶜ.Bron_Kerbosch_cliqueNum]
+      native_decide
+    · use (by infer_instance)
+      simp [readG6Header]
+  · simp [-Set.toFinset_card]
+    intros _ G GisXY _ cardeqe
+    rw [← cardeqe]
+    have h1 := Prop₄ GisXY
+    simp [-Set.toFinset_card] at h1
+    have σub : σ_G GisXY < 4 := by
+      simp +arith [σ_G, RamseyOld₂]
+      obtain ⟨v, vmindeg⟩ := G.exists_minimal_degree_vertex
+      have := (Lemma₂ G v GisXY).right
+      simp +arith [R37, ← vmindeg] at this
+      assumption
+    have h2 := G_degreeCount_eq G GisXY
+    have h3 := G_vertCount_eq G GisXY
+    have bilinearlb := σ_helper GisXY σub (λ d ↦ (e 3 7 (26 - vᵢ 2 7 d - 1) + (vᵢ 2 7 d)^2) * (G.sᵢ 2 7 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 4; assumption; simp [R37])
+    have vertexlb := σ_helper GisXY σub (λ d ↦  1 * (G.sᵢ 2 7 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 4; assumption; simp [R37])
+    have degreelb := σ_helper GisXY σub (λ d ↦ (vᵢ 2 7 d) * (G.sᵢ 2 7 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 4; assumption; simp [R37])
+    simp at bilinearlb vertexlb degreelb
+    -- NOTE: Using simp here maxes out hearbeats
+    simp_rw [bilinearlb, Finset.sum_range, Fin.sum_univ_four, vᵢ, RamseyOld₂ 7] at h1
+    simp_rw [Nat.mul_comm, degreelb, G.sum_degrees_eq_twice_card_edges, Finset.sum_range, Fin.sum_univ_four, vᵢ, RamseyOld₂] at h2
+    simp_rw [vertexlb, Finset.sum_range, Fin.sum_univ_four] at h3
+    simp at h1 h2 h3 ⊢
+    nlinarith [e37_18, e37_19, e37_20, e37_21]
 
 lemma Ineq₁₀_helper {G : SimpleGraph (Fin 28)} [DecidableRel G.Adj] (hxy: G.isXYGraph 3 8) : G.edgeFinset.card ≥ 88 := by
   have h1 := Prop₄ hxy
