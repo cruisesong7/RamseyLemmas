f1e7855f4519705341566e65f0823972d35123e8
Congyan (Cruise) Song <115286861+cruisesong7@users.noreply.github.com>
2025-09-01 15:03:03 -0400

    axiomtization & e37

--- Ramsey2lemmas/Inequalities.lean
+++ Ramsey2lemmas/Inequalities.lean
@@ -41,19 +41,36 @@ lemma sᵢ_vanish {N x y : ℕ} {G : SimpleGraph (Fin N.succ)} [DecidableRel G.A
     simp [Nat.sub_eq_iff_eq_add (Nat.le_of_lt vdegpos)] at vdeg
     simp [vdeg, Nat.add_one_le_iff, Nat.sub_add_comm (G.minDegree_le_degree v), RamseyOld₂] at σltd
 
-theorem R36 : RamseyOld 3 6 = 17 := sorry --TODO: computation K or R (the upperbound is exact so we should prove the exact values)
-theorem R37 : RamseyOld 3 7 = 22 := sorry --TODO: computation O
+axiom R36ub : RamseyOld 3 6 ≤ 17 --TODO: computation K or R
 
-theorem e3_6_11_11 : e 3 6 11 ≥ 11 := sorry --TODO: the proof start in pp.163 Appendix B
-theorem e3_6_12_15 : e 3 6 12 ≥ 15 := sorry
-theorem e3_6_13_20 : e 3 6 13 ≥ 20 := sorry
-theorem e3_6_14_25 : e 3 6 14 ≥ 25 := sorry
-theorem e3_6_15_32 : e 3 6 15 ≥ 32 := sorry --TODO: computation M
-theorem e3_6_16_40 : e 3 6 16 ≥ 40 := sorry --TODO: computation L
+theorem R36 : RamseyOld 3 6 = 17 := by
+  simp [Nat.eq_iff_le_and_ge, R36ub]
+  apply le_csSup
+  simp [isXYGraph_bddAbove]
+  let G := readG6 "P??GaOo`XMGsIcT?pSLaGuA?"
+  use G
+  unfold SimpleGraph.isXYGraph
+  simp [G.Bron_Kerbosch_cliqueNum, ← G.cliqueNum_compl, Gᶜ.Bron_Kerbosch_cliqueNum]
+  native_decide
 
-lemma e3_7_19_44 : e 3 7 19 ≥ 44 := by sorry
-lemma e3_7_20_50 : e 3 7 20 ≥ 50 := by sorry
-lemma e3_7_21_59 : e 3 7 21 ≥ 59 := by sorry
+axiom R37ub : RamseyOld 3 7 ≤ 22 --TODO: computation O
+
+theorem R37 : RamseyOld 3 7 = 22 := by
+  simp [Nat.eq_iff_le_and_ge, R37ub]
+  apply le_csSup
+  simp [isXYGraph_bddAbove]
+  let G := readG6 "UsaCB@AQAGI?HGIABGOKJCcoa[``o_U[?LcGD[O?"
+  use G
+  unfold SimpleGraph.isXYGraph
+  simp [G.Bron_Kerbosch_cliqueNum, ← G.cliqueNum_compl, Gᶜ.Bron_Kerbosch_cliqueNum]
+  native_decide
+
+axiom e3_6_11_11 : e 3 6 11 ≥ 11 --TODO: the proof start in pp.163 Appendix B
+axiom e3_6_12_15 : e 3 6 12 ≥ 15
+axiom e3_6_13_20 : e 3 6 13 ≥ 20
+axiom e3_6_14_25 : e 3 6 14 ≥ 25
+axiom e3_6_15_32 : e 3 6 15 ≥ 32  --TODO: computation M
+axiom e3_6_16_40 : e 3 6 16 ≥ 40  --TODO: computation L
 
 lemma R38lb : 27 ∈ {N | ∃ G : SimpleGraph (Fin N), G.isXYGraph 3 8} := by
   let G := readG6 "Z????CDO?a@PHA_HcE_dc`PCXQ@PoSWo@_cDS_YQQB_Qo?TLSO?q_g?{p?_?"
@@ -98,6 +115,120 @@ lemma e3_7_18_36 : e 3 7 18 ≥ 36 := by
     simp_rw [vertexlb, Finset.sum_range, Fin.sum_univ_six] at h3
     simp at h1 h2 h3 ⊢
     nlinarith [e3_6_11_11, e3_6_12_15, e3_6_13_20, e3_6_14_25, e3_6_15_32, e3_6_16_40]
+
+-- set_option maxHeartbeats 400000
+-- set_option diagnostics true
+lemma e3_7_19_44 : e 3 7 19 ≥ 44 := by
+  simp only [e]
+  apply le_csInf
+  · let G := readG6 "S?C_jO@?g`@CBEWFEACXB_pjAKeGwF`C?"
+    use G.edgeFinset.card
+    simp
+    use G
+    apply And.intro
+    · unfold SimpleGraph.isXYGraph
+      rw [G.Bron_Kerbosch_cliqueNum, ← G.cliqueNum_compl, Gᶜ.Bron_Kerbosch_cliqueNum]
+      native_decide
+    · use (by infer_instance)
+      simp [readG6Header]
+  · simp [-Set.toFinset_card]
+    intros _ G GisXY _ cardeqe
+    rw [← cardeqe]
+    have h1 := Prop₄ GisXY
+    simp [-Set.toFinset_card] at h1
+    have σub : σ_G GisXY < 5 := by
+      simp +arith [σ_G, RamseyOld₂]
+      obtain ⟨v, vmindeg⟩ := G.exists_minimal_degree_vertex
+      have := (Lemma₂ G v GisXY).right
+      simp +arith [R36, ← vmindeg] at this
+      assumption
+    have h2 := G_degreeCount_eq G GisXY
+    have h3 := G_vertCount_eq G GisXY
+    have bilinearlb := σ_helper GisXY σub (λ d ↦ (e 3 6 (19 - vᵢ 2 6 d - 1) + (vᵢ 2 6 d)^2) * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 5; assumption; simp[R36])
+    have vertexlb := σ_helper GisXY σub (λ d ↦  1 * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 5; assumption; simp [R36])
+    have degreelb := σ_helper GisXY σub (λ d ↦ (vᵢ 2 6 d) * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 5; assumption; simp [R36])
+    simp at bilinearlb vertexlb degreelb
+    -- NOTE: Using simp here maxes out hearbeats
+    simp_rw [bilinearlb, Finset.sum_range, Fin.sum_univ_five, vᵢ, RamseyOld₂ 6] at h1
+    simp_rw [Nat.mul_comm, degreelb, G.sum_degrees_eq_twice_card_edges, Finset.sum_range, Fin.sum_univ_five, vᵢ, RamseyOld₂] at h2
+    simp_rw [vertexlb, Finset.sum_range, Fin.sum_univ_five] at h3
+    simp at h1 h2 h3 ⊢
+    sorry --TODO: nlinarith failed, nlinarith! times out
+    -- nlinarith [e3_6_12_15, e3_6_13_20, e3_6_14_25, e3_6_15_32, e3_6_16_40]
+
+lemma e3_7_20_50 : e 3 7 20 ≥ 50 := by
+  simp only [e]
+  apply le_csInf
+  · let G := readG6 "T?Dg??EGI?aLAY_IIIM?cKoSiDTCLCmOBJO?"
+    use G.edgeFinset.card
+    simp
+    use G
+    apply And.intro
+    · unfold SimpleGraph.isXYGraph
+      rw [G.Bron_Kerbosch_cliqueNum, ← G.cliqueNum_compl, Gᶜ.Bron_Kerbosch_cliqueNum]
+      native_decide
+    · use (by infer_instance)
+      simp [readG6Header]
+  · simp [-Set.toFinset_card]
+    intros _ G GisXY _ cardeqe
+    rw [← cardeqe]
+    have h1 := Prop₄ GisXY
+    simp [-Set.toFinset_card] at h1
+    have σub : σ_G GisXY < 4 := by
+      simp +arith [σ_G, RamseyOld₂]
+      obtain ⟨v, vmindeg⟩ := G.exists_minimal_degree_vertex
+      have := (Lemma₂ G v GisXY).right
+      simp +arith [R36, ← vmindeg] at this
+      assumption
+    have h2 := G_degreeCount_eq G GisXY
+    have h3 := G_vertCount_eq G GisXY
+    have bilinearlb := σ_helper GisXY σub (λ d ↦ (e 3 6 (20 - vᵢ 2 6 d - 1) + (vᵢ 2 6 d)^2) * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 4; assumption; simp[R36])
+    have vertexlb := σ_helper GisXY σub (λ d ↦  1 * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 4; assumption; simp [R36])
+    have degreelb := σ_helper GisXY σub (λ d ↦ (vᵢ 2 6 d) * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 4; assumption; simp [R36])
+    simp at bilinearlb vertexlb degreelb
+    -- NOTE: Using simp here maxes out hearbeats
+    simp_rw [bilinearlb, Finset.sum_range, Fin.sum_univ_four, vᵢ, RamseyOld₂ 6] at h1
+    simp_rw [Nat.mul_comm, degreelb, G.sum_degrees_eq_twice_card_edges, Finset.sum_range, Fin.sum_univ_four, vᵢ, RamseyOld₂] at h2
+    simp_rw [vertexlb, Finset.sum_range, Fin.sum_univ_four] at h3
+    simp at h1 h2 h3 ⊢
+    nlinarith [e3_6_13_20, e3_6_14_25, e3_6_15_32, e3_6_16_40]
+
+lemma e3_7_21_59 : e 3 7 21 ≥ 59 := by
+  simp only [e]
+  apply le_csInf
+  · let G := readG6 "UsaCB@AQAGI?HGIABGOKJCcoa[``o_U[?LcGD[O?"
+    use G.edgeFinset.card
+    simp
+    use G
+    apply And.intro
+    · unfold SimpleGraph.isXYGraph
+      rw [G.Bron_Kerbosch_cliqueNum, ← G.cliqueNum_compl, Gᶜ.Bron_Kerbosch_cliqueNum]
+      native_decide
+    · use (by infer_instance)
+      simp [readG6Header]
+  · simp [-Set.toFinset_card]
+    intros _ G GisXY _ cardeqe
+    rw [← cardeqe]
+    have h1 := Prop₄ GisXY
+    simp [-Set.toFinset_card] at h1
+    have σub : σ_G GisXY < 3 := by
+      simp +arith [σ_G, RamseyOld₂]
+      obtain ⟨v, vmindeg⟩ := G.exists_minimal_degree_vertex
+      have := (Lemma₂ G v GisXY).right
+      simp +arith [R36, ← vmindeg] at this
+      assumption
+    have h2 := G_degreeCount_eq G GisXY
+    have h3 := G_vertCount_eq G GisXY
+    have bilinearlb := σ_helper GisXY σub (λ d ↦ (e 3 6 (21 - vᵢ 2 6 d - 1) + (vᵢ 2 6 d)^2) * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 3; assumption; simp[R36])
+    have vertexlb := σ_helper GisXY σub (λ d ↦  1 * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 3; assumption; simp [R36])
+    have degreelb := σ_helper GisXY σub (λ d ↦ (vᵢ 2 6 d) * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 3; assumption; simp [R36])
+    simp at bilinearlb vertexlb degreelb
+    -- NOTE: Using simp here maxes out hearbeats
+    simp_rw [bilinearlb, Finset.sum_range, Fin.sum_univ_three, vᵢ, RamseyOld₂ 6] at h1
+    simp_rw [Nat.mul_comm, degreelb, G.sum_degrees_eq_twice_card_edges, Finset.sum_range, Fin.sum_univ_three, vᵢ, RamseyOld₂] at h2
+    simp_rw [vertexlb, Finset.sum_range, Fin.sum_univ_three] at h3
+    simp at h1 h2 h3 ⊢
+    nlinarith [e3_6_14_25, e3_6_15_32, e3_6_16_40]
 
 theorem Ineq₉: e 3 8 26 ≥ 80 := by
   simp only [e]
@@ -215,9 +346,8 @@ e 3 8 28 ≥ 99 := by
       simp +arith [σ_G, RamseyOld₂]
       obtain ⟨v, vmindeg⟩ := G.exists_minimal_degree_vertex
       have := (Lemma₂ G v GisXY).right
-      simp +arith [← vmindeg] at this
-      have := R37
-      omega
+      simp +arith [R37, ← vmindeg] at this
+      assumption
     have h2 := G_degreeCount_eq G GisXY
     have h3 := G_vertCount_eq G GisXY
     have bilinearlb := σ_helper GisXY σub (λ d ↦ (e 3 7 (28 - vᵢ 2 7 d - 1) + (vᵢ 2 7 d)^2) * (G.sᵢ 2 7 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 2; assumption; simp [R37])
@@ -242,7 +372,7 @@ theorem R39Ineq {G : SimpleGraph (Fin 36)} [DecidableRel G.Adj] (hxy: G.isXYGrap
   have h3 := G_vertCount_eq G hxy
 
   have R38ub : RamseyOld 3 8 ≤ 29 := by
-    sorry --TODO: either use UNSAT proof or try formalize the informal argument on pp.154
+    sorry --TODO: formalize the informal argument on pp.154
   have R38lb : RamseyOld 3 8 ≥ 27 := by
     simp[RamseyOld]
     apply le_csSup
@@ -279,48 +409,48 @@ theorem R39Ineq {G : SimpleGraph (Fin 36)} [DecidableRel G.Adj] (hxy: G.isXYGrap
     simp at h1 h2 h3 ⊢
     nlinarith [Ineq₉, Ineq₁₀ (by simp [R38]), Ineq₁₁ (by simp [R38])]
 
--- theorem R39_36_graph_IsRegular8 {G : SimpleGraph (Fin 36)} [DecidableRel G.Adj] (hxy: G.isXYGraph 3 9):
---   G.IsRegularOfDegree 8 := by
---   simp[IsRegularOfDegree]
---   have h_neighbor := R3y_neighbor_Ind 8 hxy
+theorem R39_36_graph_IsRegular8 {G : SimpleGraph (Fin 36)} [DecidableRel G.Adj] (hxy: G.isXYGraph 3 9):
+  G.IsRegularOfDegree 8 := by
+  simp[IsRegularOfDegree]
+  have h_neighbor := R3y_neighbor_Ind G hxy
 
---   have deg_le: ∀ p ∈ Finset.univ, G.degree p ≤ 8 := by
---     intro p
---     by_contra!
---     simp[isXYGraph, cliqueNum, indepNum] at hxy
---     have hy := (csSup_le_iff' G.fintype_indepNum_bddAbove).mp (Nat.le_pred_of_lt hxy.2)
---     simp at hy
---     specialize hy (G.degree p) (G.neighborFinset p)
---     specialize h_neighbor p
---     linarith [hy h_neighbor]
+  have deg_le: ∀ p ∈ Finset.univ, G.degree p ≤ 8 := by
+    intro p
+    by_contra!
+    simp[isXYGraph, cliqueNum, indepNum] at hxy
+    have hy := (csSup_le_iff' G.fintype_indepNum_bddAbove).mp (Nat.le_pred_of_lt hxy.2)
+    simp at hy
+    specialize hy (G.degree p) (G.neighborFinset p)
+    specialize h_neighbor p
+    linarith [hy h_neighbor]
 
---   have _ := R39Ineq hxy
---   have h1 : 2 * 144 ≤ 2 * #G.edgeFinset := by linarith
---   rw[← sum_degrees_eq_twice_card_edges] at h1
---   norm_num at h1
---   have h2 : ∑ v : Fin 36, G.degree v ≤ ∑ p : Fin 36, 8 := by
---     apply Finset.sum_le_sum
---     exact deg_le
+  have _ := R39Ineq hxy
+  have h1 : 2 * 144 ≤ 2 * G.edgeFinset.card := by linarith
+  rw[← sum_degrees_eq_twice_card_edges] at h1
+  norm_num at h1
+  have h2 : ∑ v : Fin 36, G.degree v ≤ ∑ p : Fin 36, 8 := by
+    apply Finset.sum_le_sum
+    exact deg_le
 
---   have sum_deg_eq : ∑ v : Fin 36, G.degree v = ∑ p : Fin 36, 8 := by norm_num at *; linarith
---   have := (Finset.sum_eq_sum_iff_of_le deg_le).mp sum_deg_eq
---   simp at this
---   exact this
+  have sum_deg_eq : ∑ v : Fin 36, G.degree v = ∑ p : Fin 36, 8 := by norm_num at *; linarith
+  have := (Finset.sum_eq_sum_iff_of_le deg_le).mp sum_deg_eq
+  simp at this
+  exact this
 
--- theorem R39_36_graph_has_R38_27 {G : SimpleGraph (Fin 36)} [DecidableRel G.Adj] (hxy: G.isXYGraph 3 9):
---  ∃ (s : Finset (Fin 36)),s.card = 27 ∧ ((G.induce s).isXYGraph 3 8) := by
---   use Gᶜ.neighborFinset 0
---   simp [SimpleGraph.degree_compl, R39_36_graph_IsRegular8 hxy 0]
---   simp[isXYGraph]
---   by_contra! absurd
---   simp [isXYGraph] at *
---   by_cases hclique : (induce (↑(Gᶜ.neighborFinset 0)) G).cliqueNum < 3
---   specialize absurd hclique
---   suffices (induce (↑(Gᶜ.neighborFinset 0)) G).indepNum ≤ G.cliqueNum - 1 by
---     omega
---   simp [indepNum, cliqueNum]
---   -- apply csInf_le
---   sorry
---   sorry
+theorem R39_36_graph_has_R38_27 {G : SimpleGraph (Fin 36)} [DecidableRel G.Adj] (hxy: G.isXYGraph 3 9):
+ ∃ (s : Finset (Fin 36)),s.card = 27 ∧ ((G.induce s).isXYGraph 3 8) := by
+  use Gᶜ.neighborFinset 0
+  simp [SimpleGraph.degree_compl, R39_36_graph_IsRegular8 hxy 0]
+  simp[isXYGraph]
+  by_contra! absurd
+  simp [isXYGraph] at *
+  by_cases hclique : (induce (↑(Gᶜ.neighborFinset 0)) G).cliqueNum < 3
+  specialize absurd hclique
+  suffices (induce (↑(Gᶜ.neighborFinset 0)) G).indepNum ≤ G.cliqueNum - 1 by
+    omega
+  simp [indepNum, cliqueNum]
+  -- apply csInf_le
+  sorry
+  sorry
 
 end SimpleGraph
