16e44191c55084ecb2ce3cf84f105597353e4145
David E. Narváez <david.narvaez@computer.org>
2025-09-11 15:54:12 +0200

    Turn the e(3,7,19) Lemma into an Axiom
    We cannot prove this one analitically.

--- Ramsey2lemmas/Inequalities.lean
+++ Ramsey2lemmas/Inequalities.lean
@@ -119,45 +119,7 @@ lemma e3_7_18_36 : e 3 7 18 ≥ 36 := by
     simp at h1 h2 h3 ⊢
     nlinarith [e3_6_11_11, e3_6_12_15, e3_6_13_20, e3_6_14_25, e3_6_15_32, e3_6_16_40]
 
--- set_option maxHeartbeats 400000
--- set_option diagnostics true
-lemma e3_7_19_44 : e 3 7 19 ≥ 44 := by
-  simp only [e]
-  apply le_csInf
-  · let G := readG6 "S?C_jO@?g`@CBEWFEACXB_pjAKeGwF`C?"
-    use G.edgeFinset.card
-    simp
-    use G
-    apply And.intro
-    · unfold SimpleGraph.isXYGraph
-      rw [G.Bron_Kerbosch_cliqueNum, ← G.cliqueNum_compl, Gᶜ.Bron_Kerbosch_cliqueNum]
-      native_decide
-    · use (by infer_instance)
-      simp [readG6Header]
-  · simp [-Set.toFinset_card]
-    intros _ G GisXY _ cardeqe
-    rw [← cardeqe]
-    have h1 := Prop₄ GisXY
-    simp [-Set.toFinset_card] at h1
-    have σub : σ_G GisXY < 5 := by
-      simp +arith [σ_G, RamseyOld₂]
-      obtain ⟨v, vmindeg⟩ := G.exists_minimal_degree_vertex
-      have := (Lemma₂ G v GisXY).right
-      simp +arith [R36, ← vmindeg] at this
-      assumption
-    have h2 := G_degreeCount_eq G GisXY
-    have h3 := G_vertCount_eq G GisXY
-    have bilinearlb := σ_helper GisXY σub (λ d ↦ (e 3 6 (19 - vᵢ 2 6 d - 1) + (vᵢ 2 6 d)^2) * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 5; assumption; simp[R36])
-    have vertexlb := σ_helper GisXY σub (λ d ↦  1 * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 5; assumption; simp [R36])
-    have degreelb := σ_helper GisXY σub (λ d ↦ (vᵢ 2 6 d) * (G.sᵢ 2 6 d)) (by simp only [Finset.filter_eq_empty_iff]; intros; apply sᵢ_vanish GisXY _ 5; assumption; simp [R36])
-    simp at bilinearlb vertexlb degreelb
-    -- NOTE: Using simp here maxes out hearbeats
-    simp_rw [bilinearlb, Finset.sum_range, Fin.sum_univ_five, vᵢ, RamseyOld₂ 6] at h1
-    simp_rw [Nat.mul_comm, degreelb, G.sum_degrees_eq_twice_card_edges, Finset.sum_range, Fin.sum_univ_five, vᵢ, RamseyOld₂] at h2
-    simp_rw [vertexlb, Finset.sum_range, Fin.sum_univ_five] at h3
-    simp at h1 h2 h3 ⊢
-    sorry --TODO: nlinarith failed, nlinarith! times out
-    -- nlinarith [e3_6_12_15, e3_6_13_20, e3_6_14_25, e3_6_15_32, e3_6_16_40]
+axiom e3_7_19_44 : e 3 7 19 ≥ 44
 
 lemma e3_7_20_50 : e 3 7 20 ≥ 50 := by
   simp only [e]
